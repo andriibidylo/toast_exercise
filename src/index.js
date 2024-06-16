d2167871b8d4e2e57fe273e45da126a7fae30eb0
a <a@as-MacBook-Pro.local>
2024-06-16 11:39:25 -0700

    chore/update react and mui

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import CssBaseline from '@mui/material/CssBaseline';
+import ReactDOM from "react-dom/client";
+import CssBaseline from "@mui/material/CssBaseline";
 
-import App from './App';
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
     <CssBaseline />
     <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
+  </>
 );
