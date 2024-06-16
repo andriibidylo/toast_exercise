5b012813d39833ee764a5dca81fe314aabd1769c
a <a@as-MacBook-Pro.local>
2024-06-16 14:26:11 -0700

    feature/add disable Toast when clicked

--- src/components/FormSubmission.jsx
+++ src/components/FormSubmission.jsx
@@ -5,6 +5,7 @@ import { removeFormSubmissionById } from "../utils/removeFormSubmissionById";
 
 export const FormSubmission = ({ mutateLinkedSubmission }) => {
   const [currentFormsSubmissions, setCurrentFormsSubmissions] = useState([]);
+  const [isDisableToast, setIsDisableToast] = useState(false);
 
   useEffect(() => {
     const handleMessage = (message) => {
@@ -15,11 +16,15 @@ export const FormSubmission = ({ mutateLinkedSubmission }) => {
   }, [setCurrentFormsSubmissions]);
 
   const handleLike = (currentToast) => {
+    setIsDisableToast(true);
     mutateLinkedSubmission(currentToast, {
       onSuccess: () => {
         setCurrentFormsSubmissions((prev) =>
           removeFormSubmissionById(prev, currentToast.id)
         );
+      },
+      onSettled: () => {
+        setIsDisableToast(false);
       },
     });
   };
@@ -34,6 +39,7 @@ export const FormSubmission = ({ mutateLinkedSubmission }) => {
             key={form.id}
             message={`${form.data?.firstName} ${form.data?.lastName} ${form.data?.email}`}
             open={true}
+            isDisable={isDisableToast}
             onLike={() => handleLike(form)}
             onClose={() => handleClose(form.id)}
           />
--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -4,14 +4,25 @@ import IconButton from "@mui/material/IconButton";
 import CloseIcon from "@mui/icons-material/Close";
 import { Box, SnackbarContent } from "@mui/material";
 
-export const Toast = ({ message, open, onLike, onClose }) => {
+export const Toast = ({
+  message,
+  open,
+  onLike,
+  onClose,
+  isDisable = false,
+}) => {
   return (
     <Box sx={{ width: 500 }}>
       <Snackbar open={open}>
         <SnackbarContent
           action={
             <>
-              <Button color="inherit" size="small" onClick={() => onLike()}>
+              <Button
+                color="inherit"
+                size="small"
+                disabled={isDisable}
+                onClick={() => onLike()}
+              >
                 Like
               </Button>
 
@@ -19,6 +30,7 @@ export const Toast = ({ message, open, onLike, onClose }) => {
                 size="small"
                 aria-label="close"
                 color="inherit"
+                disabled={isDisable}
                 onClick={() => onClose()}
               >
                 <CloseIcon fontSize="small" />
@@ -27,6 +39,7 @@ export const Toast = ({ message, open, onLike, onClose }) => {
           }
           message={message}
           sx={{
+            backgroundColor: isDisable ? "#71797E" : "#1876D1",
             color: "white",
           }}
         ></SnackbarContent>
