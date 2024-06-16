02a440cd5c0e1a5d08035a6e0350fa634f264f62
a <a@as-MacBook-Pro.local>
2024-06-16 14:03:18 -0700

    feature/add refetch liked list after press like

--- src/query/useSaveLikedSubmission.js
+++ src/query/useSaveLikedSubmission.js
@@ -1,7 +1,8 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { saveLikedFormSubmission } from "../service/mockServer";
 
 export const useSaveLikedSubmission = () => {
+  const queryClient = useQueryClient();
 
   const mutationFn = async (currentToast) => {
     await saveLikedFormSubmission(currentToast);
@@ -10,5 +11,8 @@ export const useSaveLikedSubmission = () => {
   return useMutation({
     mutationFn,
     mutationKey: ["save_liked_submission"],
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["liked_submissions"] });
+    },
   });
 };
