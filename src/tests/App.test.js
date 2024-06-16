c99f0d850901f942a5bc5353b7a8e360438727fc
a <a@as-MacBook-Pro.local>
2024-06-16 16:17:55 -0700

    chore/update test dependencis and add babel

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { screen, render } from '@testing-library/react';
 import App from '../App';
 
+
 test('renders header text', () => {
+
   render(<App />);
 
   const heading  = screen.getByRole('heading', { name: /toast exercise/i});
